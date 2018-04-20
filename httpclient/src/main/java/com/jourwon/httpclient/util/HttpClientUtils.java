b010be25fd2f51eb839ca9cca6593f238d4a7f11
JourWon <JourWon@163.com>
2018-04-20 22:38:15 +0800

    增加http请求头设置

--- httpclient/src/main/java/com/jourwon/httpclient/util/HttpClientUtils.java
+++ httpclient/src/main/java/com/jourwon/httpclient/util/HttpClientUtils.java
@@ -44,30 +44,40 @@ public class HttpClientUtils {
 	
 	// 请求获取数据的超时时间(即响应时间)，单位毫秒。
 	private static final int SOCKET_TIMEOUT = 6000;
-	
-	
 
 	/**
-	 * 发送get请求；不带请求参数
+	 * 发送get请求；不带请求头和请求参数
 	 * 
 	 * @param url 请求地址
-	 * @param params 参数集合
 	 * @return
 	 * @throws Exception
 	 */
 	public static HttpClientResult doGet(String url) throws Exception {
-		return doGet(url, null);
+		return doGet(url, null, null);
 	}
-
+	
 	/**
 	 * 发送get请求；带请求参数
 	 * 
 	 * @param url 请求地址
-	 * @param params 参数集合
+	 * @param params 请求参数集合
 	 * @return
 	 * @throws Exception
 	 */
 	public static HttpClientResult doGet(String url, Map<String, String> params) throws Exception {
+		return doGet(url, null, params);
+	}
+
+	/**
+	 * 发送get请求；带请求头和请求参数
+	 * 
+	 * @param url 请求地址
+	 * @param headers 请求头集合
+	 * @param params 请求参数集合
+	 * @return
+	 * @throws Exception
+	 */
+	public static HttpClientResult doGet(String url, Map<String, String> headers, Map<String, String> params) throws Exception {
 		// 创建httpClient对象
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 
@@ -90,6 +100,9 @@ public class HttpClientUtils {
 		 */
 		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(CONNECT_TIMEOUT).setSocketTimeout(SOCKET_TIMEOUT).build();
 		httpGet.setConfig(requestConfig);
+		
+		// 设置请求头
+		packageHeader(headers, httpGet);
 
 		// 创建httpResponse对象
 		CloseableHttpResponse httpResponse = null;
@@ -104,17 +117,16 @@ public class HttpClientUtils {
 	}
 
 	/**
-	 * 发送post请求；不带请求参数
+	 * 发送post请求；不带请求头和请求参数
 	 * 
 	 * @param url 请求地址
-	 * @param params 参数集合
 	 * @return
 	 * @throws Exception
 	 */
 	public static HttpClientResult doPost(String url) throws Exception {
-		return doPost(url, null);
+		return doPost(url, null, null);
 	}
-
+	
 	/**
 	 * 发送post请求；带请求参数
 	 * 
@@ -124,6 +136,19 @@ public class HttpClientUtils {
 	 * @throws Exception
 	 */
 	public static HttpClientResult doPost(String url, Map<String, String> params) throws Exception {
+		return doPost(url, null, params);
+	}
+
+	/**
+	 * 发送post请求；带请求头和请求参数
+	 * 
+	 * @param url 请求地址
+	 * @param headers 请求头集合
+	 * @param params 请求参数集合
+	 * @return
+	 * @throws Exception
+	 */
+	public static HttpClientResult doPost(String url, Map<String, String> headers, Map<String, String> params) throws Exception {
 		// 创建httpClient对象
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 
@@ -144,7 +169,8 @@ public class HttpClientUtils {
 		httpPost.setHeader("Accept-Language", "zh-CN,zh;q=0.9");
 		httpPost.setHeader("Accept-Encoding", "gzip, deflate, br");
 		httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");*/
-
+		packageHeader(headers, httpPost);
+		
 		// 封装请求参数
 		packageParam(params, httpPost);
 
@@ -234,6 +260,22 @@ public class HttpClientUtils {
 
 		params.put("_method", "delete");
 		return doPost(url, params);
+	}
+	
+	/**
+	 * Description: 封装请求头
+	 * @param params
+	 * @param httpMethod
+	 */
+	public static void packageHeader(Map<String, String> params, HttpRequestBase httpMethod) {
+		// 封装请求头
+		if (params != null) {
+			Set<Entry<String, String>> entrySet = params.entrySet();
+			for (Entry<String, String> entry : entrySet) {
+				// 设置到请求头到HttpRequestBase对象中
+				httpMethod.setHeader(entry.getKey(), entry.getValue());
+			}
+		}
 	}
 
 	/**
--- httpclient/src/test/java/com/jourwon/httpclient/test/HttpClientUtilsTest.java
+++ httpclient/src/test/java/com/jourwon/httpclient/test/HttpClientUtilsTest.java
@@ -41,13 +41,19 @@ public class HttpClientUtilsTest {
 	}
 
 	/**
-	 * Description: 测试post无参请求
+	 * Description: 测试post带请求头不带请求参数
 	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void testPost() throws Exception {
-		HttpClientResult result = HttpClientUtils.doPost("http://127.0.0.1:8080/hello/post");
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put("Cookie", "123");
+		headers.put("Connection", "keep-alive");
+		headers.put("Accept", "application/json");
+		headers.put("Accept-Language", "zh-CN,zh;q=0.9");
+		headers.put("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+		HttpClientResult result = HttpClientUtils.doPost("http://127.0.0.1:8080/hello/post", headers, null);
 		System.out.println(result);
 	}
 
